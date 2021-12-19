1e36ea1fe65682600f0a535446ee30cfe7c01740
MCarbono <carbono.marcelo@gmail.com>
2021-12-18 21:57:36 -0300

    feat: user data interface

---
+++ src/useCases/register-user-on-mailing-list/registerUserOnMailingList.spec.ts
@@ -0,0 +1,14 @@
+import { UserData } from './userData'
+
+describe('Register user on mailing list use case', () => {
+    test('Should add user with complete data to mailing list', async () => {
+        const users: UserData[] = [];
+        // const repo: UserRepository = new InMemoryUserRepository(users);
+        // const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+        // const name = 'any_name'
+        // const email = 'any@email.com'
+        // const response = await useCase.registerUserOnMailingList({ name, email }) 
+        // const user = await repo.findUserByEmail('any@email.com')
+        // expect(user.name).toBe('any_name')
+    })
+})
