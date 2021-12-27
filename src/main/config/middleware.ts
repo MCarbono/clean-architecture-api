efbc85d10dbed234beb9437c99b1c014bd2717f5
MCarbono <carbono.marcelo@gmail.com>
2021-12-27 19:10:31 -0300

    feat: express router adapter

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,7 +1,7 @@
 import { Express } from 'express';
-import { bodyParser } from './middleware/bodyParser';
-import { contentType } from './middleware/contentType';
-import { cors } from './middleware/cors';
+import { bodyParser } from '../middleware/bodyParser';
+import { contentType } from '../middleware/contentType';
+import { cors } from '../middleware/cors';
 
 export default (app: Express): void => {
     app.use(bodyParser)
