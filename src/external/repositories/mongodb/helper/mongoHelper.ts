6566e8f13da4f9e066a18057852b463c4cb59b1e
MCarbono <carbono.marcelo@gmail.com>
2021-12-28 20:46:22 -0300

    feat: mongoUserRepository with tests

---
+++ src/external/repositories/mongodb/helper/mongoHelper.ts
@@ -0,0 +1,21 @@
+import { Collection, MongoClient } from 'mongodb'
+const mongoClient = require('mongodb').MongoClient;
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  async connect(uri: string): Promise<void> {
+    this.client = await mongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+  async disconnect(): Promise<void> {
+    this.client.close()
+  },
+  getCollection(name: string): any {
+    return this.client.db().collection(name)
+  },
+  clearCollection(name: string): void {
+    this.client.db().collection(name).deleteMany({})
+  }
+}
---
+++ src/external/repositories/mongodb/mongodbUserRepository.ts
@@ -0,0 +1,24 @@
+import { UserData } from "@/entities";
+import { UserRepository } from "@/useCases/register-user-on-mailing-list/ports";
+import { MongoHelper } from "./helper";
+
+export class MongodbUserRepository implements UserRepository {
+    async add(user: UserData): Promise<void> {
+        const userCollection = MongoHelper.getCollection('users')
+        const exists = await this.exists(user)
+        if(!exists) await userCollection.insertOne(user)
+    }
+    async findUserByEmail(email: string): Promise<UserData> {
+        const userCollection = MongoHelper.getCollection('users')
+        const result = await userCollection.findOne({ email })
+        return result;
+    }
+    async findAllUsers(): Promise<UserData[]> {
+        return await MongoHelper.getCollection('users').find().toArray();
+    }
+    async exists(user: UserData): Promise<boolean> {
+        const result = await this.findUserByEmail(user.email)
+        if(!result) return false;
+        return true;
+    }
+}
