cb2b812cc783e1098a7e1ddd4e3a0bcb5c712d6b
MCarbono <carbono.marcelo@gmail.com>
2021-12-29 01:24:34 -0300

    test: send email when name and email are valid

--- test/useCases/send-email/sendEmail.spec.ts
+++ test/useCases/send-email/sendEmail.spec.ts
@@ -1,12 +1,48 @@
-const attachmentFilePath = '../resources/text.txt'
-const fromName = 'Test'
-const fromEmail = 'from_mail@mail.com'
-const toName = 'any_name'
-const toEmail = 'to_email@mail.com'
-const subject = 'Test e-mail'
-const emailBody = 'Hello World attachment test'
-const emailBodyHtml = '<b>Hello WOrld attachment test</b>'
+import { Either, Right, right } from "@/shared";
+import { EmailOptions, EmailService } from "@/useCases/send-email/ports/emailService";
+import { MailServiceError } from "@/useCases/send-email/errors/mailServiceError";
+import { SendEmail } from "@/useCases/send-email";
+
+const attachmentFilePath = '../resources/text.txt';
+const fromName = 'Test';
+const fromEmail = 'from_mail@mail.com';
+const toName = 'any_name';
+const toEmail = 'to_email@mail.com';
+const subject = 'Test e-mail';
+const emailBody = 'Hello World attachment test';
+const emailBodyHtml = '<b>Hello WOrld attachment test</b>';
 const attachment = [{
     filename: attachmentFilePath,
     contentType: 'text/plain'
-}]
+}];
+
+const mailOptions: EmailOptions = {
+    host: 'test',
+    port: 867,
+    username: 'test',
+    password: 'test',
+    from: `fromName ${fromEmail}`,
+    to: `toName <${toEmail}>`,
+    subject,
+    text: emailBody,
+    html: emailBodyHtml,
+    attachments: attachment
+}
+
+class MailServiceStub implements EmailService {
+    async send(emailOptions: EmailOptions): Promise<Either<MailServiceError, EmailOptions>> {
+        return right(emailOptions)
+    }
+}
+
+describe('Send email to user', () => {
+    test('Should email user with valid name and email address', async () => {
+        const mailServiceStub = new MailServiceStub()
+        const useCase = new SendEmail(mailOptions, mailServiceStub)
+        const response = await useCase.perform({
+            name: toName, 
+            email: toEmail
+        })
+        expect(response).toBeInstanceOf(Right)
+    })
+})
