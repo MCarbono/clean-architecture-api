9d7b261217f168e2e91a1fc8d6eae540c893e9f9
MCarbono <carbono.marcelo@gmail.com>
2021-12-28 21:45:02 -0300

    test: connect to database mongodb

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,10 +1,12 @@
 import { RegisterUserController } from "@/webControllers";
 import { RegisterUserOnMailingList } from "@/useCases/register-user-on-mailing-list";
-import { InMemoryUserRepository } from "@/useCases/register-user-on-mailing-list/repository";
+//import { InMemoryUserRepository } from "@/useCases/register-user-on-mailing-list/repository";
+import { MongodbUserRepository } from "@/external/repositories/mongodb";
 
 export const makeRegisterUserController = (): RegisterUserController => {
-    const inMemoryUserRepository = new InMemoryUserRepository([])
-    const registerUserOnMailingList = new RegisterUserOnMailingList(inMemoryUserRepository)
+    //const inMemoryUserRepository = new InMemoryUserRepository([])
+    const mongodbUserRepository = new MongodbUserRepository()
+    const registerUserOnMailingList = new RegisterUserOnMailingList(mongodbUserRepository)
     const registerUserController = new RegisterUserController(registerUserOnMailingList)
     return registerUserController
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,28 @@
-require('module-alias/register')
+import 'dotenv/config'
+import 'module-alias/register';
+import { MongoHelper } from '@/external/repositories/mongodb/helper';
 import app from '../main/config/app';
 
 const port = 5000;
-app.listen(port, () => {
-    console.log(`Server on. http://localhost:${port}`)
-})
+
+// MongoHelper.connect(process.env.MONGO_URI)
+//     .then(async () => {
+//         const app = (await import('./config/app')).default
+//         app.listen(port, () => {
+//             console.log(`Server on. http://localhost:${port}`)
+//         })
+//     })
+//     .catch(console.error)
+
+const start = async () => {
+    try {
+        await MongoHelper.connect(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server on. http://localhost:${port}`)
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
